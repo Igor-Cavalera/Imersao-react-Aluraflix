0280e2a20030c0db53ec8dd212bb63eda0fb29a3
Igor Cavalera <igorcavalera455@gmail.com>
2020-08-01 04:44:12 -0300

    Ok

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/video'
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+//Desafio master blaster wtf
+const Pagina404 = () => (<div>Página 404</div>)
+
+  ReactDOM.render(
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Home} exact/>
+        <Route path="/cadastro/video" component={CadastroVideo} />
+        <Route path="/cadastro/categoria" component={CadastroCategoria} />
+        <Route path="/" component={Pagina404}/>
+      </Switch>
+    </BrowserRouter>,
+    document.getElementById('root')
+  );
 
